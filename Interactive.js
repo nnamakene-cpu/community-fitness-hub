71f092edfb543ecf55754b059db10afa4adbd70b
nnamakene-cpu <nnamakene@gmail.com>
2026-06-23 14:54:33 +0100

    Create Interactive.js

---
+++ Interactive.js
@@ -0,0 +1,44 @@
+function pageLoaded () {
+alert(Welcome to the Interactive Features page. ");
+}
+function pageLeft () {
+alert("You are leaving the Interactive Features page. ");
+}
+function changeText) {
+const text = document.getElementById("demoText");
+text.innerHTML = "The text has been changed using JavaScript.";
+text.style.color = "green";
+text.style.fontweight = "bold";
+function hidelmage) {
+document. getElementById("fitnessImage"). style.display = "none";
+function showimage) {
+document. getElementById("fitnessImage"). style.display - "block";
+function moveBox) {
+let box - document. getElementById ("movingBox"); let position - 0;
+let interval - setInterval(function) {
+if (position › 300) { clearInterval (interval);
+} else {
+position += 5;
+box. style.left = position + "px";
+3, 20);
+function hoverEffect) {
+const hoverText - document. gettlementById("hoverText");
+hoverText.style.color = "blue";
+hoverText.style.fontSize = "22px";
+}
+function validateForm) {
+const name - document.getElementById("memberName"). value;
+const membership - document.getElementById("membershipType"). value;
+const formMessage - document.getlementById(" formMessage");
+if ( name === "" || membership === "") {
+forMessage.innerHTML = " Please complete all form fields. ";
+formMessage.style.color = "red";
+return false;
+}
+formMessage. innerHTML = "Form submitted successfully.";
+formMessage.style.color = "green";
+return false;
+}
+function textSelected() {
+alert ("You selected the text in the input field.");
+}
